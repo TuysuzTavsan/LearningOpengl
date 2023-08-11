285946e47a7d68cea87633d77c7aecc04ac70734
tuysuztavsan <fuat.kilic.afg@gmail.com>
2023-08-11 05:25:00 +0300

    changed camera class so it uses my version of look at matrix. (Camera class homeworks are done ! TODO: COLOR section)

--- Headers/Camera.h
+++ Headers/Camera.h
@@ -26,9 +26,10 @@ private:
 
 	glm::vec3 Position;
 	glm::vec3 Front;
-	glm::vec3 Up;
-	glm::vec3 Right;
+	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 Right = glm::vec3(1.0f, 0.0f, 0.0f);
 	glm::vec3 WorldUp;
+	glm::vec3 LookAt = glm::vec3(0.0f, 0.0f, 0.0f);
 
 	float Yaw;
 	float Pitch;
@@ -42,7 +43,7 @@ public:
 		float yaw = YAW, float pitch = PITCH);
 	Camera(float posX, float posY, float posZ, float Upx, float UpY, float UpZ, float yaw, float pitch);
 	~Camera();
-	
+
 	glm::mat4 GetViewMatrix();
 	void ProcessKeyboard(Camera_Movement direction, float deltaTime);
 	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
--- Src/Camera.cpp
+++ Src/Camera.cpp
@@ -36,6 +36,27 @@ Camera::~Camera()
 
 glm::mat4 Camera::GetViewMatrix()
 {
+	glm::vec3 zaxis = glm::normalize(Position - (Position + Front));
+	glm::vec3 xaxis = Right;  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+	glm::vec3 yaxis = Up;
+
+	glm::mat4 translation = glm::mat4(1.0f); // Identity matrix by default
+	translation[3][0] = -Position.x; // Third column, first row
+	translation[3][1] = -Position.y;
+	translation[3][2] = -Position.z;
+	glm::mat4 rotation = glm::mat4(1.0f);
+	rotation[0][0] = xaxis.x; // First column, first row
+	rotation[1][0] = xaxis.y;
+	rotation[2][0] = xaxis.z;
+	rotation[0][1] = yaxis.x; // First column, second row
+	rotation[1][1] = yaxis.y;
+	rotation[2][1] = yaxis.z;
+	rotation[0][2] = zaxis.x; // First column, third row
+	rotation[1][2] = zaxis.y;
+	rotation[2][2] = zaxis.z;
+
+	return rotation * translation;
+
 	return glm::lookAt(Position, Position + Front, Up);
 }
 
@@ -59,7 +80,7 @@ void Camera::ProcessMouseMovement(float xoffset, float yoffset, GLboolean constr
 
 	Yaw += xoffset;
 	Pitch += yoffset;
-
+	
 	// make sure that when pitch is out of bounds, screen doesn't get flipped
 	if (constrainPitch)
 	{
@@ -72,6 +93,7 @@ void Camera::ProcessMouseMovement(float xoffset, float yoffset, GLboolean constr
 	// update Front, Right and Up Vectors using the updated Euler angles
 	updateCameraVectors();
 }
+
 
 void Camera::ProcessMouseScroll(float yoffset)
 {
@@ -90,7 +112,14 @@ void Camera::updateCameraVectors()
 	front.y = sin(glm::radians(Pitch));
 	front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
 	Front = glm::normalize(front);
-	// also re-calculate the Right and Up vector
+	//also re-calculate the Right and Up vector
+
+	
 	Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
 	Up = glm::normalize(glm::cross(Right, Front));
+
+	
+
+	
+	
 }
